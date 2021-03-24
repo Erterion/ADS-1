ffd8674211405eb85f19c3f3b20c3c6b01485694
Erterion <79190617+Erterion@users.noreply.github.com>
2021-03-24 12:31:07 +0300

    Акимкин Алексей

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,29 +1,22 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-int mid, left, right, k,chet;
-left = 0;
-right = size-1;
-mid = size / 2;
-k = value;
-chet = 0;
-while (*(arr + mid) != k) {
-if (*(arr + mid) > k) {
-left = mid + 1;
-mid = (right - mid) / 2;
-}
-else {
-right = mid;
-mid = (left + mid) / 2;
-}}
-k = mid;
-while (*(arr+k-1)==value) {
-chet += 1;
-k -= 1;
-}
-k = mid;
-while (*(arr + k + 1) == value) {
-chet += 1;
-k += 1;}
- return(chet+1);
+int left, right, mid, kolvo;
+    left = 0;
+    right = size - 1;
+    kolvo = 0;
+    while (left < right) {
+        mid = (left+right) / 2;
+        if (*(arr+mid) < value)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    if (*(arr + left) == value) {
+        while (*(arr+left) == value) {
+            kolvo++;
+            left++;
+        }
+    }
+        return kolvo;
 }
